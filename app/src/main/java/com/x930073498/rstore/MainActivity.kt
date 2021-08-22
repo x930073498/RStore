a5bd0c1892d22368fb32d102a52af0bccacd534f
x930073498 <x930073498@gmail.com>
2021-08-22 22:48:01 +0800

    rename some method Name

--- app/src/main/java/com/x930073498/rstore/MainActivity.kt
+++ app/src/main/java/com/x930073498/rstore/MainActivity.kt
@@ -5,33 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.x930073498.rstore.databinding.ActivityMainBinding
-
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity(), StoreComponent {
-    private val viewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
-
-    @SuppressLint("SetTextI18n")
+    private val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setState(binding)
     }
-
     private fun setState(binding:  ActivityMainBinding) {
         viewModel.withAnchor {
             with(it) {
                 onInitialized {
-                    binding.root.setOnClickListener {
-                        data = "${++count}"
-                    }
+                    binding.root.setOnClickListener { data = "${++count}" }
                 }
-                stareAt(::data) {
-                    binding.data.text = "data=$data"
-                }
+                stareAt(::data) { binding.data.text = "data=$data" }
             }
         }
     }
-
 }
