9fc6db162dfb4328dd2ade3b770ad0f9056fcade
x930073498 <x930073498@gmail.com>
2021-09-17 14:13:11 +0800

    修改部分实现

--- features/src/main/java/com/x930073498/features/Api.kt
+++ features/src/main/java/com/x930073498/features/Api.kt
@@ -22,13 +22,13 @@ import com.x930073498.features.internal.Features
 val application by ::app
 
 
-fun requireFeatureInit(context: Context) {
+fun requireFeatureInit(context: Context) =
     AppInitializer.getInstance(context).initializeComponent(FeatureStartup::class.java)
-}
 
-fun addInitializer(initializer: Initializer) {
+
+fun addInitializer(initializer: Initializer) =
     Features.addInitializer(initializer)
-}
+
 
 fun addLifecycleObserver(
     applicationFeatureLifecycleObserver: ApplicationFeatureLifecycleObserver = ApplicationFeatureLifecycleObserver,
@@ -57,41 +57,38 @@ fun addFragmentLifecycleObserver(observer: FragmentFeatureLifecycleObserver) =
 fun addApplicationLifecycleObserver(observer: ApplicationFeatureLifecycleObserver) =
     addInitializer(LifecycleInitializer(applicationFeatureLifecycleObserver = observer))
 
-inline fun <reified F : IFeature> installInstanceFeature(noinline action: F.(FeatureTarget) -> Unit) {
+inline fun <reified F : IFeature> installInstanceFeature(noinline action: F.(FeatureTarget) -> Unit) =
     addInitializer(InstanceFeatureInitializer(F::class.java, action))
-}
+
 
 fun <T : Activity, F : IFeature> installActivityFeature(
     clazz: Class<T>,
     feature: F,
     action: ActivityTargetData<T, F>.() -> Unit
-) {
-    addInitializer(TargetClassInitializer(clazz, feature) {
-        action(this as ActivityTargetData<T, F>)
-    })
-}
+) = addInitializer(TargetClassInitializer(clazz, feature) {
+    action(this as ActivityTargetData<T, F>)
+})
+
 
 fun <T : Fragment, F : IFeature> installFragmentFeature(
     clazz: Class<T>,
     feature: F,
     action: FragmentTargetData<T, F>.() -> Unit
-) {
-    addInitializer(
-        TargetClassInitializer(
-            clazz,
-            feature
-        ) { action(this as FragmentTargetData<T, F>) })
-}
+) = addInitializer(
+    TargetClassInitializer(
+        clazz,
+        feature
+    ) { action(this as FragmentTargetData<T, F>) })
+
 
 fun <F : IFeature> installApplicationFeature(
     feature: F,
     action: ApplicationTargetData<Application, F>.() -> Unit
-) {
-    addInitializer(
-        TargetClassInitializer(
-            Application::class.java,
-            feature
-        ) { action(this as ApplicationTargetData<Application, F>) })
-}
+) = addInitializer(
+    TargetClassInitializer(
+        Application::class.java,
+        feature
+    ) { action(this as ApplicationTargetData<Application, F>) })
+
 
 
